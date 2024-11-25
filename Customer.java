6745a977aeb2b693c25fded0453deb837e34cf70
Beckerziin <joao_becker7@hotmail.com>
2024-11-25 20:50:08 -0300

    Commit 5

--- Customer.java
+++ Customer.java
@@ -26,25 +26,24 @@ public class Customer {
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
 
-         // calculate amount for each rental
-         double thisAmount = each.getCharge();
-
          // add frequent renter points
          frequentRenterPoints++;
          // add bonus for a two-day new release rental
          if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-             each.getDaysRented() > 1) frequentRenterPoints++;
+             each.getDaysRented() > 1) {
+            frequentRenterPoints++;
+         }
 
          // show figures for this rental
          result += "\t" + each.getMovie().getTitle() + "\t" +
-                 String.valueOf(thisAmount) + "\n";
-         totalAmount += thisAmount;
+                   String.valueOf(each.getCharge()) + "\n";
+         totalAmount += each.getCharge();
       }
 
       // add footer lines
       result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
       result += "You earned " + String.valueOf(frequentRenterPoints) +
-              " frequent renter points";
+                " frequent renter points";
       return result;
    }
 
